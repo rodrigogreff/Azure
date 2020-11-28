788934385bea680d838133d923c23de7ff25c4f7
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2020-11-27 21:45:01 -0300

    Update Install-IIS.ps1

--- Install-IIS.ps1
+++ Install-IIS.ps1
@@ -9,3 +9,19 @@ remove-item  C:\inetpub\wwwroot\iisstart.htm
 
 #Allow ICMPV4
 New-NetFirewallRule -DisplayName AllowICMPv4 -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow
+
+#Desligar a VM 
+Stop-AzureRmVM -ResourceGroupName "RG-VALIERE" -Name "SRV-API" -Force
+
+#Generalizar a VM
+Set-AzureRmVM -ResourceGroupName "RG-VALIERE" -Name "SRV-API" -Generalized
+
+# Get na VM 
+$vm = Get-AzureRmVM -Name "SRV-API" -ResourceGroupName "RG-VALIERE"
+
+# Criação da "Image Custom" baseada na configuração da VM
+$image = New-AzureRmImageConfig -Location "eastus2" -SourceVirtualMachineId $vm.ID 
+
+# Criar "Image Custom"
+New-AzureRmImage -Image $image -ImageName "SRV-API-IMAGE-V1" -ResourceGroupName "RG-VALIERE"
+

fc940d99f1521932f10d15c7976b23275ce64ab5
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-01-16 20:06:22 -0300

    Update Install-ADDSForest_SRV_2022.ps1

--- Install-ADDSForest_SRV_2022.ps1
+++ Install-ADDSForest_SRV_2022.ps1
@@ -2,18 +2,24 @@
 # Windows PowerShell script for AD DS Deployment 2022
 #
 
-Install-WindowsFeature -Name AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+#Declare variables
+$DatabasePath = "c:\windows\NTDS"
+$DomainMode = "WinThreshold"
+$DomainName = "rodrigogreff.local"
+$DomaninNetBIOSName = "RODRIGOGREFF"
+$ForestMode = "WinThreshold"
+$LogPath = "c:\windows\NTDS"
+$SysVolPath = "c:\windows\SYSVOL"
+$featureLogPath = "c:\poshlog\featurelog.txt" 
+$Password = (ConvertTo-SecureString -String "P@ssw0rd" -AsPlainText -Force)
 
-Import-Module ADDSDeployment
-Install-ADDSForest `
--CreateDnsDelegation:$false `
--DatabasePath "C:\Windows\NTDS" `
--DomainMode "WinThreshold" `
--DomainName "rodrigogreff.local" `
--DomainNetbiosName "RODRIGOGREFF" `
--ForestMode "WinThreshold" `
--InstallDns:$true `
--LogPath "C:\Windows\NTDS" `
--NoRebootOnCompletion:$false `
--SysvolPath "C:\Windows\SYSVOL" `
--Force:$true
+#Install AD DS, DNS and GPMC 
+start-job -Name addFeature -ScriptBlock { 
+Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools } 
+Wait-Job -Name addFeature 
+Get-WindowsFeature | Where installed gt;&gt;$featureLogPath
+
+#Create New AD Forest
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath $DatabasePath -DomainMode $DomainMode -DomainName $DomainName -SafeModeAdministratorPassword $Password -DomainNetbiosName $DomainNetBIOSName -ForestMode $ForestMode -InstallDns:$true -LogPath $LogPath -NoRebootOnCompletion:$false -SysvolPath $SysVolPath -Force:$true

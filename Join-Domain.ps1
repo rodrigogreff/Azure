71fc6541bab2026bfc2078ec62e847b5d3f94dec
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-11-19 00:02:27 -0300

    Add files via upload

---
+++ Join-Domain.ps1
@@ -0,0 +1,13 @@
+﻿# Parâmetros
+param (
+    [string]$DomainName,
+    [string]$DomainUser,
+    [string]$DomainPassword
+)
+
+# Converte a senha em um objeto seguro
+$SecurePassword = ConvertTo-SecureString $DomainPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($DomainUser, $SecurePassword)
+
+# Adiciona a máquina ao domínio
+Add-Computer -DomainName $DomainName -Credential $Credential -Force -Restart

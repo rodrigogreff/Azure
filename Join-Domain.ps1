045a576693488ba36dc4ef4bda63fa4efea98a57
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-11-19 15:59:30 -0300

    Update Join-Domain.ps1

--- Join-Domain.ps1
+++ Join-Domain.ps1
@@ -1,6 +1,6 @@
 # Parâmetros do domínio
 $DomainName = "greffcode.local" # Substitua pelo nome do seu domínio
-$DomainUser = "adminazure" # Substitua pelo usuário do domínio com permissão de junção
+$DomainUser = "adminazure@greffcode.local" # Substitua pelo usuário do domínio com permissão de junção
 $DomainPassword = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force # Substitua pela senha do usuário do domínio
 
 # Criar credenciais do domínio

4146a7baad89db02ddfc2207cf29f6d0c4e30c05
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-11-19 16:11:10 -0300

    Update Join-Domain.ps1

--- Join-Domain.ps1
+++ Join-Domain.ps1
@@ -1,10 +1,13 @@
-# Parâmetros do domínio
-$DomainName = "greffcode.local" # Substitua pelo nome do seu domínio
-$DomainUser = "adminazure@greffcode.local" # Substitua pelo usuário do domínio com permissão de junção
-$DomainPassword = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force # Substitua pela senha do usuário do domínio
+# Parâmetros
+param (
+    [string]$DomainName,
+    [string]$DomainUser,
+    [string]$DomainPassword
+)
 
-# Criar credenciais do domínio
-$DomainCredential = New-Object System.Management.Automation.PSCredential ($DomainUser, $DomainPassword)
+# Converte a senha em um objeto seguro
+$SecurePassword = ConvertTo-SecureString $DomainPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($DomainUser, $SecurePassword)
 
-# Adicionar a máquina ao domínio
-Add-Computer -DomainName $DomainName -Credential $DomainCredential -Restart -Force
+# Adiciona a máquina ao domínio
+Add-Computer -DomainName $DomainName -Credential $Credential -Force -Restart

8fb796fca9ac2f273eb4f091735a4642342748d9
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-11-20 00:53:14 -0300

    Update Create-Domain-Users.ps1

--- Create-Domain-Users.ps1
+++ Create-Domain-Users.ps1
@@ -14,14 +14,23 @@ if (Test-Path $csvPath) {
     exit 1
 }
 
-# Criar OU
-New-ADOrganizationalUnit -Name "GREFFCode" -Path "DC=greffcode,DC=local"
-New-ADOrganizationalUnit -Name "Users" -Path "OU=GREFFCode,DC=greffcode,DC=local"
+# Criar OU, caso ainda não exista
+$ouGREFFCode = Get-ADOrganizationalUnit -Filter 'Name -eq "GREFFCode"' -ErrorAction SilentlyContinue
+if (-not $ouGREFFCode) {
+    Write-Host "Criando OU GREFFCode."
+    New-ADOrganizationalUnit -Name "GREFFCode" -Path "DC=greffcode,DC=local"
+}
 
+$ouUsers = Get-ADOrganizationalUnit -Filter 'Name -eq "Users"' -SearchBase "OU=GREFFCode,DC=greffcode,DC=local" -ErrorAction SilentlyContinue
+if (-not $ouUsers) {
+    Write-Host "Criando OU Users."
+    New-ADOrganizationalUnit -Name "Users" -Path "OU=GREFFCode,DC=greffcode,DC=local"
+}
 
-# Importar lista de usuários a partir do arquivo CSV
+# Importar lista de usuários a partir do arquivo CSV usando csvde
+Write-Host "Importando usuários."
 csvde -i -f $csvPath -k
 
 # Alterar senha e habilitar a conta
+Write-Host "Alterando senhas e habilitando contas."
 dsquery user "ou=Users,ou=GREFFCode,dc=greffcode,dc=local" | dsmod user -pwd "P@ssw0rd" -disabled no
-
